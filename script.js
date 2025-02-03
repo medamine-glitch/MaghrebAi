574d17e150b6c9ac27f1a8d849af059d0b7aad24
medamine-glitch <139164967+medamine-glitch@users.noreply.github.com>
2025-02-03 17:07:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 // Handle contact form submission.
-<script src="https://app.dante-ai.com/bubble-embed.js?kb_id=5472d20a-d4e3-4596-bb72-99e393b70244&token=b088002f-4cd2-4d89-8038-f7e078051909&modeltype=gpt-4-omnimodel&tabs=false"></script>
-  
+  let menuList = document.getElementById("nav-links");
+      menuList.style.maxHeight = "0px";
+
+      function toggleMenu() {
+        if (menuList.style.maxHeight == "0px") {
+          menuList.style.maxHeight = "300px";
+        } else {
+          menuList.style.maxHeight = "0px";
+        }
+      }
